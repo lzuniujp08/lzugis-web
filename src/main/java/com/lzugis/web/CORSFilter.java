c2590536c0dcfeb6f428bfcefd28da57b86b898d
lzugis <1004740957@qq.com>
2018-06-20 10:11:39 +0800

    URL转发跨域设置

---
+++ src/main/java/com/lzugis/web/CORSFilter.java
@@ -0,0 +1,36 @@
+package com.lzugis.web;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class CORSFilter implements Filter {
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        /* 允许跨域的主机地址 */
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        /* 允许跨域的请求方法GET, POST, HEAD 等 */
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        /* 重新预检验跨域的缓存时间 (s) */
+        response.setHeader("Access-Control-Max-Age", "3600");
+        /* 允许跨域的请求头 */
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        /* 是否携带cookie */
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        chain.doFilter(req, res);
+    }
+
+    public void init(FilterConfig filterConfig) {}
+
+    public void destroy() {}
+
+}
